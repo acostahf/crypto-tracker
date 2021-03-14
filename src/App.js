2d3af81b528209ad8e6b33076913db4d7abe5a5d
acostahf <acostahf4@gmail.com>
2021-03-14 13:10:28 -0500

    looping through data to display crypto market

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Coin from "./Coin";
 
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
@@ -18,14 +20,39 @@ function App() {
       .catch((error) => console.log(error));
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text"></h1>
         <form>
-          <input type="text" placeholder="search" className="coin-input" />
+          <input
+            type="text"
+            placeholder="search"
+            className="coin-input"
+            onChange={handleChange}
+          />
         </form>
       </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+          />
+        );
+      })}
     </div>
   );
 }
