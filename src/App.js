799d1b817219461de31dd3dc8152436d61ab11ac
acostahf <acostahf4@gmail.com>
2021-03-14 14:35:30 -0500

    css completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,44 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import Coin from "./Coin";
+import { makeStyles } from "@material-ui/core/styles";
 
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
+const useStyles = makeStyles((theme) => ({
+  coinApp: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    paddingTop: "64px",
+    backgroundColor: "#212121",
+    color: "#ffffff",
+  },
+  coinSearch: {
+    marginBottom: "64px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  coinText: {
+    marginBottom: "32px",
+    textAlign: "center",
+  },
+  coinInput: {
+    paddingLeft: "16px",
+    width: "300px",
+    height: "50px",
+    borderRadius: "4px",
+    border: "none",
+    backgroundImage: "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)",
+    color: "#e2e2e2",
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
 
@@ -29,32 +63,34 @@ function App() {
   );
 
   return (
-    <div className="coin-app">
-      <div className="coin-search">
-        <h1 className="coin-text"></h1>
-        <form>
-          <input
-            type="text"
-            placeholder="search"
-            className="coin-input"
-            onChange={handleChange}
-          />
-        </form>
+    <div className="App">
+      <div className={classes.coinApp}>
+        <div className={classes.coinSearch}>
+          <h1 className={classes.coinText}> Search a Crypto Currency</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="search"
+              className={classes.coinInput}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              image={coin.image}
+              price={coin.current_price}
+              priceChange={coin.price_change_percentage_24h}
+              marketcap={coin.total_volume}
+              symbol={coin.symbol}
+              volume={coin.market_cap}
+            />
+          );
+        })}
       </div>
-      {filteredCoins.map((coin) => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            image={coin.image}
-            price={coin.current_price}
-            priceChange={coin.price_change_percentage_24h}
-            marketcap={coin.total_volume}
-            symbol={coin.symbol}
-            volume={coin.market_cap}
-          />
-        );
-      })}
     </div>
   );
 }
--- src/Coin.js
+++ src/Coin.js
@@ -1,4 +1,64 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  coinContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  coinRow: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "start",
+    alignItems: "center",
+    height: "80px",
+    borderBottom: "solid",
+    width: "900px",
+  },
+  coin: {
+    display: "flex",
+    alignItems: "center",
+    paddingRight: "24px",
+    minWidth: "300px",
+    color: "#fff",
+  },
+  h1: {
+    fontSize: "16px",
+    width: "150px",
+  },
+  img: {
+    height: "30px",
+    width: "30px",
+    marginRight: "10px",
+  },
+  symbol: {
+    textTransform: "uppercase",
+    width: "80px",
+  },
+  data: {
+    display: "flex",
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  price: {
+    width: "80px",
+  },
+  volume: {
+    width: "230px",
+  },
+  marketcap: {
+    width: "230px",
+  },
+  percentPos: {
+    width: "80px",
+    color: "green",
+  },
+  percentNeg: {
+    width: "80px",
+    color: "red",
+  },
+}));
 
 const Coin = ({
   name,
@@ -9,22 +69,23 @@ const Coin = ({
   priceChange,
   marketcap,
 }) => {
+  const classes = useStyles();
   return (
-    <div className="coin-container">
-      <div className="coin-row">
-        <div className="coin">
-          <img src={image} alt="crypto" />
-          <h1>{name}</h1>
-          <p className="coin-symbol">{symbol}</p>
-          <div className="coin-data">
-            <p className="coin-price">${price}</p>
-            <p className="coin-volume">${volume.toLocaleString()}</p>
+    <div className={classes.coinContainer}>
+      <div className={classes.coinRow}>
+        <div className={classes.coin}>
+          <img className={classes.img} src={image} alt="crypto" />
+          <h1 className={classes.h1}>{name}</h1>
+          <p className={classes.symbol}>{symbol}</p>
+          <div className={classes.data}>
+            <p className={classes.price}>${price}</p>
+            <p className={classes.volume}>${volume.toLocaleString()}</p>
             {priceChange < 0 ? (
-              <p className="coin-percent red">{priceChange.toFixed(2)}% </p>
+              <p className={classes.percentPos}>{priceChange.toFixed(2)}% </p>
             ) : (
-              <p className="coin-percent green">{priceChange.toFixed(2)} %</p>
+              <p className={classes.percentNeg}>{priceChange.toFixed(2)} %</p>
             )}
-            <p className="coin-marketcap">
+            <p className={classes.marketcap}>
               Mkt Cap: ${marketcap.toLocaleString()}
             </p>
           </div>
